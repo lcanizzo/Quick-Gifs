4486738d177494401a1d7982ec74b5a4197b8999
unknown <luca.canizzo@gmail.com>
2017-09-02 18:21:13 -0400

    Added Recent Searches button logic, input field api requests, and input val resets

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,17 +6,17 @@ var limit = 20;                                         //  U P D A T E    T O  
 // Set rating limit for returns data objects
 var rating = "pg";
 // Set array of recommended search queries 
-var recArray = ["Puppies", "Kitten", "Dog", "Huskies", "Cat", "Dance", "Sweet","Bernie", "Fallon"];
-
+var recArray = ["Puppies", "Kitten", "Dog", "Huskies", "Cat"];
+var recentSearches = [];
 $(document).ready(function () {
-// Create buttons for recommended search queries, class="gif-btn rec-btn"
+// Create buttons for recommended search queries, class="gif-btn"
     for (let i = 0; i < recArray.length; i++) {
         var btn = $("<button>");
-        btn.addClass("btn btn-outline-secondary gif-btn rec-btn");
+        let capital = recArray[i].toUpperCase();
+        btn.addClass("btn btn-outline-secondary gif-btn");
         btn.attr("data-name", recArray[i]);
-        btn.text(recArray[i]);
+        btn.text(capital);
         $(".rec-box").append(btn);
-        // console.log("data-name:", btn.data("name"))
     }
 
 // On click event for class="gif-btn" buttons in document
@@ -60,6 +60,41 @@ $(document).ready(function () {
         }
     })
 
-
-
+// On keypress event for input field ("#search")
+    $("#search").on("keyup", function(e){
+        let input = $(this).val().trim();
+        console.log(input);        
+        //If "enter" pressed, run api function
+        if(e.which == 13) {
+            let queryUrl = giphyUrl + "&q=" + input + "&rating=" + rating + "&limit=" + limit;
+            $.ajax({
+                url: queryUrl,
+                method: "GET"
+            }).done((result)=>{
+                $("#gif-box").empty();
+                console.log('result:', result);
+                for(let i = 0; i < result.data.length; i++) {
+                    var gif = $("<img>");
+                    $(gif).addClass("loaded-gif");
+                    $(gif).attr('src', result.data[i].images.fixed_height_still.url);
+                    $(gif).attr('data-state', "still");
+                    $(gif).attr('data-animate', result.data[i].images.fixed_height.url);
+                    $(gif).attr('data-still', result.data[i].images.fixed_height_still.url);     
+                    $('#gif-box').prepend(gif);
+                }
+            })  
+            $("#search").val(" ");
+        }
+        //If input has not been searched recently, add to recentSearches and make recent searches button
+        if(e.which == 13 && recentSearches.indexOf(input)== -1) {
+            let capital = input.toUpperCase();
+            var recentBtn = $("<button>");
+            recentBtn.addClass("btn btn-outline-secondary gif-btn");
+            recentBtn.attr("data-name", input);
+            recentBtn.text(capital);
+            $(".search-box").append(recentBtn);
+            recentSearches.push(input);
+            console.log(recentSearches);
+        }
+    })
 })
