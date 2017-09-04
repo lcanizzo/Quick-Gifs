67e9bb2e2387ce1a240a672866bd44f49935eafc
unknown <luca.canizzo@gmail.com>
2017-09-04 16:00:48 -0400

    Added to readme.md, added rating input and functionality

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Positive-Gifs
-A Website to search through Gifs
+A responsive website to search through Gifs with clear Gif ratings (Y, G, PG, PG-13, R)
+
+###### This website runs api requests and dynamically replaces content based on user input, it uses:
+- jQuery & jQuery UI
+- Bootstrap
+- CSS & html
+- Giphy api
+
+###### The focus of this website is to allow users to search for gifs in an organized manner.
+- Recent user searches are logged as buttons in a subsection below recommended searches.
+- Ratings for each gif are displayed to the left or above it.
+- Users can update the number of gifs that will be displayed and the maximum rating the of the gifs.
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,7 @@
-
 const apiKey = "75ba12d6350f4824ac414e0486808405";
 const giphyUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey;
 // Set limit for number of data objects returned in JSON 
-var limit = 10;                                       
+var limit = 10;
 // Set rating limit for returns data objects
 var rating = "r";
 // Set array of recommended search queries 
@@ -10,7 +9,7 @@ var recArray = ["Puppies", "Kitten", "Dog", "Huskies", "Cat"];
 var recentSearches = [];
 
 $(document).ready(function () {
-// Create buttons for recommended search queries, class="gif-btn"
+    // Create buttons for recommended search queries, class="gif-btn"
     for (let i = 0; i < recArray.length; i++) {
         var btn = $("<button>");
         let capital = recArray[i].toUpperCase();
@@ -22,19 +21,68 @@ $(document).ready(function () {
 
 //Set search limit
     //Set on enter:
-$("#limit").on("keyup", function(e){
-    let input = $(this).val().trim();
-    //If "enter" pressed, change limit number
-    if(e.which == 13) {
-        limit = input
-    }
-})
+    $("#limit").on("keyup", function (e) {
+        let input = $(this).val().trim();
+        //If "enter" pressed, change limit number
+        if (e.which == 13) {
+            limit = input
+        }
+    })
     //Set on click:
-$("#limit-button").on("click", function(){
-    let input = $("#limit").val().trim();
-    //On "click", change limit number
-    limit = input
-})
+    $("#limit-button").on("click", function () {
+        let input = $("#limit").val().trim();
+        //On "click", change limit number
+        limit = input
+    })
+
+//Set rating
+    //Set on enter:
+    $("#rated").on("keyup", function (e) {
+        let input = $(this).val().trim().toUpperCase();
+        console.log('inputRated:', input);
+        //If "enter" pressed, change rating
+        if (e.which == 13) {
+            if (input == "Y" 
+            ||
+            input == "G"
+            ||
+            input == "PG"
+            ||
+            input == "PG-13"
+            ||
+            input == "R") {
+                rating = input
+                console.log('rating:', rating);
+            } else {
+                alert("Please enter a valide rating: Y, G, PG, PG-13, R");
+            }
+        }
+    })
+    //Set on click:
+    $("#rated-button").on("click", function () {
+        let input = $("#rated").val().trim().toUpperCase();
+        console.log('inputRated:', input);        
+        //If "O.K" pressed, change rating
+        if (input == "Y" 
+        ||
+        input == "G"
+        ||
+        input == "PG"
+        ||
+        input == "PG-13"
+        ||
+        input == "R") {
+            rating = input
+            console.log('rating:', rating);
+        } else {
+            alert("Please enter a valide rating: Y, G, PG, PG-13, R");
+        }
+    })
+
+//On click event for return to top button   
+    $(".toTop").on('click', function () {
+        window.scrollTop(0, 0);
+    })
 
 // On click event for class="gif-btn" buttons in document
     // Giphy api request query= button.data-name
@@ -46,10 +94,10 @@ $("#limit-button").on("click", function(){
         $.ajax({
             url: queryUrl,
             method: "GET"
-        }).done((result)=>{
+        }).done((result) => {
             $("#gif-box").empty();
             console.log('result:', result);
-            for(let i = 0; i < result.data.length; i++) {
+            for (let i = 0; i < result.data.length; i++) {
                 var rating = $("<p>");
                 $("#gif-box").append(rating);
                 rating.addClass("ratings badge badge-pill badge-secondary");
@@ -66,12 +114,12 @@ $("#limit-button").on("click", function(){
                 $(gif).attr('src', result.data[i].images.fixed_height_still.url);
                 $(gif).attr('data-state', "still");
                 $(gif).attr('data-animate', result.data[i].images.fixed_height.url);
-                $(gif).attr('data-still', result.data[i].images.fixed_height_still.url);     
-            } 
+                $(gif).attr('data-still', result.data[i].images.fixed_height_still.url);
+            }
         })
         window.scrollTo(0, 0);
     })
-    
+
 // On click event for class="loaded-gif", toggles between animated and still src & data-state
     $(document).on("click", ".loaded-gif", function () {
         var state = $(this).attr("data-state");
@@ -87,19 +135,19 @@ $("#limit-button").on("click", function(){
     })
 
 // On keypress event for input field ("#search")
-    $("#search").on("keyup", function(e){
+    $("#search").on("keyup", function (e) {
         let input = $(this).val().trim();
         //If "enter" pressed, run api function
-        if(e.which == 13) {
+        if (e.which == 13) {
             let queryUrl = giphyUrl + "&q=" + input + "&rating=" + rating + "&limit=" + limit;
             $.ajax({
                 url: queryUrl,
                 method: "GET"
-            }).done((result)=>{
+            }).done((result) => {
                 console.log('result:', result);
                 $("#gif-box").empty();
 
-                for(let i = 0; i < result.data.length; i++) {
+                for (let i = 0; i < result.data.length; i++) {
                     var rating = $("<p>");
                     $("#gif-box").append(rating);
                     rating.addClass("ratings badge badge-pill badge-secondary");
@@ -109,22 +157,22 @@ $("#limit-button").on("click", function(){
                     } else if (result.data[i].rating == "pg-13") {
                         rating.attr('class', "ratings badge badge-pill badge-warning");
                     }
-    
+
                     var gif = $("<img>");
                     $('#gif-box').append(gif);
                     $(gif).addClass("loaded-gif");
                     $(gif).attr('src', result.data[i].images.fixed_height_still.url);
                     $(gif).attr('data-state', "still");
                     $(gif).attr('data-animate', result.data[i].images.fixed_height.url);
-                    $(gif).attr('data-still', result.data[i].images.fixed_height_still.url);   
+                    $(gif).attr('data-still', result.data[i].images.fixed_height_still.url);
                 }
-            })  
+            })
             $("#search").val(" ");
         }
 
-        //If input has not been searched recently, add to recentSearches array and make recent searches button
+    //If input has not been searched recently, add to recentSearches array and make recent searches button
 
-        if(e.which == 13 && recentSearches.indexOf(input)== -1) {
+        if (e.which == 13 && recentSearches.indexOf(input) == -1) {
             let capital = input.toUpperCase();
             var recentBtn = $("<button>");
             recentBtn.addClass("btn btn-outline-secondary gif-btn");
@@ -140,41 +188,41 @@ $("#limit-button").on("click", function(){
     $(document).on("click", "#search-button", function () {
         let input = $("#search").val().trim();
         //Run api function
-            let queryUrl = giphyUrl + "&q=" + input + "&rating=" + rating + "&limit=" + limit;
-            console.log(queryUrl);
-            $.ajax({
-                url: queryUrl,
-                method: "GET"
-            }).done((result)=>{
-                console.log('result:', result);
-                $("#gif-box").empty();
+        let queryUrl = giphyUrl + "&q=" + input + "&rating=" + rating + "&limit=" + limit;
+        console.log(queryUrl);
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).done((result) => {
+            console.log('result:', result);
+            $("#gif-box").empty();
 
-                for(let i = 0; i < result.data.length; i++) {
-                    var rating = $("<p>");
-                    $("#gif-box").append(rating);
-                    rating.addClass("ratings badge badge-pill badge-secondary");
-                    rating.text("Rated: " + result.data[i].rating.toUpperCase());
-                    if (result.data[i].rating == "r") {
-                        rating.attr('class', "ratings badge badge-pill badge-danger");
-                    } else if (result.data[i].rating == "pg-13") {
-                        rating.attr('class', "ratings badge badge-pill badge-warning");
-                    }
-    
-                    var gif = $("<img>");
-                    $('#gif-box').append(gif);
-                    $(gif).addClass("loaded-gif");
-                    $(gif).attr('src', result.data[i].images.fixed_height_still.url);
-                    $(gif).attr('data-state', "still");
-                    $(gif).attr('data-animate', result.data[i].images.fixed_height.url);
-                    $(gif).attr('data-still', result.data[i].images.fixed_height_still.url);   
+            for (let i = 0; i < result.data.length; i++) {
+                var rating = $("<p>");
+                $("#gif-box").append(rating);
+                rating.addClass("ratings badge badge-pill badge-secondary");
+                rating.text("Rated: " + result.data[i].rating.toUpperCase());
+                if (result.data[i].rating == "r") {
+                    rating.attr('class', "ratings badge badge-pill badge-danger");
+                } else if (result.data[i].rating == "pg-13") {
+                    rating.attr('class', "ratings badge badge-pill badge-warning");
                 }
-            })  
-            $("#search").val(" ");
-            window.scrollTo(0, 0);            
 
-        //If input has not been searched recently, add to recentSearches array and make recent searches button
+                var gif = $("<img>");
+                $('#gif-box').append(gif);
+                $(gif).addClass("loaded-gif");
+                $(gif).attr('src', result.data[i].images.fixed_height_still.url);
+                $(gif).attr('data-state', "still");
+                $(gif).attr('data-animate', result.data[i].images.fixed_height.url);
+                $(gif).attr('data-still', result.data[i].images.fixed_height_still.url);
+            }
+        })
+        $("#search").val(" ");
+        window.scrollTo(0, 0);
 
-        if(recentSearches.indexOf(input)== -1) {
+    //If input has not been searched recently, add to recentSearches array and make recent searches button
+
+        if (recentSearches.indexOf(input) == -1) {
             let capital = input.toUpperCase();
             var recentBtn = $("<button>");
             recentBtn.addClass("btn btn-outline-secondary gif-btn");
@@ -184,10 +232,5 @@ $("#limit-button").on("click", function(){
             recentSearches.push(input);
             console.log('recentSearches:', recentSearches);
         }
-    })
-
-    //On click event for return to top button
-    $(".toTop").on('click', function(){
-        window.scrollTop(0, 0);
     })
 })
